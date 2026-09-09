0895887fa1d150c6b837daebb08fcbc3d4079e1a
Georgii Bukharov <souljorje@gmail.com>
2026-09-09 13:08:06 +0400

    fix: render recording indicator as visible menu bar image

--- Sources/Fluid/Services/CallRecordingIndicatorController.swift
+++ Sources/Fluid/Services/CallRecordingIndicatorController.swift
@@ -25,15 +25,12 @@ final class CallRecordingIndicatorController {
         if visible {
             guard self.statusItem == nil else { return }
 
-            let item = NSStatusBar.system.statusItem(withLength: 10)
+            let item = NSStatusBar.system.statusItem(withLength: 14)
+            item.isVisible = true
             if let button = item.button {
-                button.attributedTitle = NSAttributedString(
-                    string: "●",
-                    attributes: [
-                        .foregroundColor: NSColor.systemRed,
-                        .font: NSFont.systemFont(ofSize: 7, weight: .semibold),
-                    ]
-                )
+                button.image = Self.recordingDotImage()
+                button.imagePosition = .imageOnly
+                button.imageScaling = .scaleNone
                 button.toolTip = "FluidVoice is recording a call"
             }
             self.statusItem = item
@@ -43,5 +40,15 @@ final class CallRecordingIndicatorController {
         guard let item = self.statusItem else { return }
         NSStatusBar.system.removeStatusItem(item)
         self.statusItem = nil
+    }
+
+    private static func recordingDotImage() -> NSImage {
+        let image = NSImage(size: NSSize(width: 8, height: 8))
+        image.lockFocus()
+        NSColor.systemRed.setFill()
+        NSBezierPath(ovalIn: NSRect(x: 1, y: 1, width: 6, height: 6)).fill()
+        image.unlockFocus()
+        image.isTemplate = false
+        return image
     }
 }
